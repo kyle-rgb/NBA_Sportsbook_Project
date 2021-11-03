fe96206b01d9d708c338ef248dfdcac6b443dcdd
Kyle Lewis <kylel9815@gmail.com>
2021-11-03 17:53:36 -0400

    Wrote Working Function to Scrape Time Data

--- src/data/shark.py
+++ src/data/shark.py
@@ -1,4 +1,4 @@
-import time, sys, requests, bs4, os, json, pandas as pd
+import time, sys, requests, re, bs4, os, json, pandas as pd
 from selenium import webdriver
 
 # Test For Getting Game Specific Data
@@ -17,8 +17,21 @@ matchup_links = []
 # time.sleep(5)
 # driver.quit()
 
+
+# with open("data_structure.txt", "a") as file:
+#     file.write("\n")
+#     file.write(f"{table['oddsshark_gamecenter'].keys()}")
+#     # for key, value in table["oddsshark_gamecenter"].items():
+#     #     file.write("> " + key)
+#     #     file.write("\n")
+#     #     if type(value) == dict:
+#     #         for k in value.keys():
+#     #             file.write("\t> " + str(k) + "\n")
+#     file.write("\n")
+#     file.close()
+# print(response.text)
+
 # These two tasks should be handled functionally.
-# Test For Grabbing Books Data For Sample Game
 def grabLines(url):
     # Press See More Button
     response = requests.get(url)
@@ -53,31 +66,10 @@ def grabLines(url):
             book_df.loc[i, "Spread"] = spread[k]["spread"]
             book_df.loc[i, "Odds"] = spread[k]["spread_price"]
             i+=1    
-    #book_df=book_df.fillna(method="ffill")
     print(book_df)
 
-    # with open("data_structure.txt", "a") as file:
-    #     file.write("\n")
-    #     file.write(f"{table['oddsshark_gamecenter'].keys()}")
-    #     # for key, value in table["oddsshark_gamecenter"].items():
-    #     #     file.write("> " + key)
-    #     #     file.write("\n")
-    #     #     if type(value) == dict:
-    #     #         for k in value.keys():
-    #     #             file.write("\t> " + str(k) + "\n")
-    #     file.write("\n")
-    #     file.close()
-    #print(response.text)
-    # Gather Book Table (for Moneyline and Spread)
-        # 2 Rows per Book 
-            # Top: Away - ML Odds - Spread Line (Odds)
-            # Bottom: Home - ML Odds - Spread Line (Odds)
-    # Press Total/Ou Button
-        # 2 Rows for over odds (away column) and under odds ()
-        # As Well As A Joined Column encasing the book's total number (since there is only one number here)
+def grabTimedMarkets(id):
 # Test For Grabbing Time Based Line Information From Embedded Link in Game Page
-    # Get Element on Game Page Where Link Text is Line History
-    # Will Come to a Page With Book Information and Time Information for Available Books
     # Table be structured as such:
         # Table Headers
             # Bookname
@@ -87,6 +79,26 @@ def grabLines(url):
             # Bookname: A UTC Timestamp on Lines and Odds at Specific Book at this moment in Time
             # Line (Home): Contains Both Book's Line and Associated Odds at time point
             # Over / Under: Total and Book's Assocaited Odds at time point (curiously these totals have a spread-like look to time with an explicit + sign. Will need to be cleaned)
+    market_df = pd.DataFrame()
+    columns_list = ["Line (Home)", "Over / Under"]
+    response = requests.get(f"https://www.oddsshark.com/nba/odds/line-history/{str(id)}")
+    soup = bs4.BeautifulSoup(response.text, features="lxml")
+    tables = soup.select("table")
+    df_list = pd.read_html(str(tables))
+    for df in df_list:
+        for col in df.columns:
+            if col not in columns_list:
+                df["Book"] = col
+                df = df.rename(columns={col: "Timestamp"})
+        market_df = pd.concat([market_df, df])
+    
+    market_df["game_id"] = id
+    market_df.reset_index(inplace=True, drop=True)
+    print(market_df)
 
 
-grabLines("https://www.oddsshark.com/nba/denver-utah-odds-october-26-2021-1459586")
+# Test For Grabbing Books Data For Sample Game
+#grabLines("https://www.oddsshark.com/nba/denver-utah-odds-october-26-2021-1459586")
+
+# Test For Grabbing Time Based Line Information 
+grabTimedMarkets(1459586)
